c8d2806a350af9a3af226abffcd960bfa418111b
“PennMak” <tsmak@ucsc.edu>
2022-04-19 13:05:12 +0800

    3

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// A cat game.
+// A game in which the user controls the cat to move to a specified location.
+// @link  lab4/index.html
+// @file  lab4/js/lab.js
+// @author Tszho Mak
+// @since  04/18/2022
+
+// Draw the background
+// Draw the cat
+// Give the cat location to explore
+// On each frame
+//   Accept User input to control the cat
+//   Validate User's input
+//   Print win message if cat reach the location
