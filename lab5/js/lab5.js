05ae3e0d1f2f6d19ec33f53d64b10519dd717afc
“PennMak” <tsmak@ucsc.edu>
2022-04-21 07:22:47 +0800

    Update lab5.js

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -14,8 +14,8 @@
  age = 2022 - year;
 
  //output
- document.writeln("Make: " + make + "<br>");
- document.writeln("Model: " + model + "<br>");
- document.writeln("Color: " + color + "<br>");
- document.writeln("Year: " + year + "<br>");
- document.writeln("Age: " + age + "years<br>");
+ document.writeln("Make: " + make + " <br> ");
+ document.writeln("Model: " + model + " <br> ");
+ document.writeln("Color: " + color + " <br> ");
+ document.writeln("Year: " + year + " <br> ");
+ document.writeln("Age: " + age + " years<br> ");
