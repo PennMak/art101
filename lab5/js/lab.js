26f68c5dbbe4a50458f93e63c5fe9620bededbb1
“PennMak” <tsmak@ucsc.edu>
2022-04-21 07:09:57 +0800

    update

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Tszho Mak <tsmak@ucsc.edu>
+ * Created: 4/20/2021
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make = "cat";
+ model = "Garfield";
+ color = "White";
+ year = 2019;
+
+ //calculate
+ age = 2022 - year;
+
+ //output
+ document.writeln("Make:" + make + "<br>");
+ document.writeln("Model" + model + "<br>");
+ document.writeln("Color" + color + "<br>");
+ document.writeln("Year" + year + "<br>");
+ document.writeln("Age" + age + "years<br>");
