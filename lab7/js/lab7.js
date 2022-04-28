c4230a22e56986ef886c79f494c7def90d510dc1
“PennMak” <tsmak@ucsc.edu>
2022-04-28 19:36:58 +0800

    Update lab7.js

--- lab7/js/lab7.js
+++ lab7/js/lab7.js
@@ -6,14 +6,14 @@
 
  // sortUserName - a function that takes user input and sorts the letters
  // of their name
- function sortUserName() {
+function sortUserName() {
    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
    console.log("userName =", userName);
    // split string to array
    var nameArray = userName.split('');
    console.log("nameArray =", nameArray);
    // sort the array
-   var nameArraysort = nameArray.sort();
+   var nameArraySort = nameArray.sort();
    console.log("nameArraySort =", nameArraySort);
    // join array back to string
    var nameSorted = nameArraySort.join('')
@@ -21,5 +21,5 @@
    return nameSorted;
 }
 // Output
-document.writeln("Oh hey, I have fixed your name:",
+document.writeln("Oh hey, I have fixed your name: ",
   sortUserName(), "</br>");
