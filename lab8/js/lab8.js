85721d0b8510f87cb84755063d7e7eb3f0db4c90
“PennMak” <tsmak@ucsc.edu>
2022-05-03 16:17:18 +0800

    update

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -24,3 +24,6 @@ var result = array.map(function(x){
   return x ** 0.5;
 })
 console.log("squareroot of array:", result);
+
+var outputEl = document.getElementById("output");
+outputEl.innerHTML = mapResults; // put your results here
