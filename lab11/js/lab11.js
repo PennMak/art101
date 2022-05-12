39c65ff4a80839827cb3aea86042fb37d39ae373
“PennMak” <tsmak@ucsc.edu>
2022-05-12 15:48:16 +0800

    Update lab11.js

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -1,6 +1,6 @@
 /*
  * Author: Tszho Mak <tsmak@ucsc.edu>
- * Created: 5/6/2021
+ * Created: 5/11/2021
  * License: Public Domain
  */
 
@@ -9,9 +9,40 @@
 
 // test function
 
-$("button.green").click(function(){
-  // remove possible existing class
-  $("#target").removeClass("red");
-  // now toggle red class
-  $("#target").toggleClass("green");
-});
+function init(){
+  $(document).ready(function(){
+    var cha = $('<button class="button">Challenges</button>');
+    $('#challenges').append(cha);
+    cha.on('click',function(){
+      $(this).parent().toggleClass('special')
+    });
+
+    var pro = $('<button class="button">Problem</button>');
+    $('#problem').append(pro);
+    pro.on('click',function(){
+      $(this).parent().toggleClass('special')
+    });
+
+    var res = $('<button class="button">Results</button>');
+    $('#results').append(res);
+    res.on('click',function(){
+      $(this).parent().toggleClass('special')
+    });
+
+    var dontpressme = $('<button class="button">Do Not Press Me</button>');
+    $('#output').append(dontpressme);
+    dontpressme.on('click',function(){
+      isaiddonot.addClass('special');
+      donotpressme.removeClass('special')
+    });
+
+    var isaiddonot = $('<button class="button">I said do not!</button>');
+    $('#output').append(isaiddonot);
+    isaiddonot.on('click',function(){
+      donotpressme.addClass('special');
+      isaiddonot.removeClass('special')
+    });
+
+
+  });
+}
