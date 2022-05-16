6455d6e16c425330ad1c5d65728e94c46e38188e
“PennMak” <tsmak@ucsc.edu>
2022-05-16 17:07:43 +0800

    lab 13 update

---
+++ lab13/js/lab13.js
@@ -0,0 +1,26 @@
+/*
+ * Author: Tszho Mak <tsmak@ucsc.edu>
+ * Created: 5/6/2021
+ * License: Public Domain
+ */
+
+ // sortUserName - a clickable link that directly showing lab9 index.html
+ // of their name
+
+// test function
+
+function fizzBuzzBoom(maxNums, factorObj){
+  for (var num=0; num<maxNums; num++){
+    var outputStr = "";
+    for (var factor in factorObj){
+      if (num % factor == 0){
+        outputStr += factorObj[factor];
+
+      }
+    }
+    if(outputStr){
+      outputStr = " - " + outputStr + "!";
+    }
+    outputToPage(num.toString() + outputStr)
+  }
+}
