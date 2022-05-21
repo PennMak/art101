f7be41acba641d21479799dcb0588a61e11b6901
“PennMak” <tsmak@ucsc.edu>
2022-05-21 09:55:29 +0800

    lab 14

---
+++ lab14/js/lab14.js
@@ -0,0 +1,55 @@
+/*
+ * Author: Tszho Mak <tsmak@ucsc.edu>
+ * Created: 5/6/2021
+ * License: Public Domain
+ */
+
+ // sortUserName - a clickable link that directly showing lab9 index.html
+ // of their name
+
+// test function
+
+function fizzBuzzBoom(maxNums, factorObj){
+  for (var num=0; num<maxNums; num++){
+    var outputStr = "";
+    for (var factor in factorObj){
+      if (num % factor == 0){
+        outputStr += factorObj[factor];
+
+      }
+    }
+    if(outputStr){
+      outputStr = " - " + outputStr + "!";
+    }
+    outputToPage(num.toString() + outputStr)
+  }
+}
+
+function reportError(str){
+  outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+}
+
+document.getElementById("submit").addEventListener("click",function(){
+  console.log("Button pushed.");
+  var max = document.getElementById("max").value;
+  if (! max){
+    reportError("You must provide a maximum");
+    return
+  }
+  var factorObj = getFactorObj();
+  console.log("factorObj:", factorObj);
+  if (Object.key(factorObj).length === 0){
+    reportError("You must provide at least one factor and text");
+    return;
+  }
+  console.log("Error checks passed.");
+  outputEl.innerHTML = "";
+  console.log("Running fizzBuzz..");
+  fizzBuzzBoom(max, factorObj);
+  document.writeln(foo());
+  console.log("Done running FizzBuzz.");
+  outputEl.classList.add("cols");
+})
+
+oneLongString += num + " Fizz!<br>";
+$("#output").html(oneLongString);
