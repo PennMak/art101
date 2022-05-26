9ca74add8a67764d5caa31647fcbcca53361226c
“PennMak” <tsmak@ucsc.edu>
2022-05-26 14:32:56 +0800

    update

--- lab15/js/lab15.js
+++ lab15/js/lab15.js
@@ -1,6 +1,6 @@
 /*
  * Author: Tszho Mak <tsmak@ucsc.edu>
- * Created: 5/6/2021
+ * Created: 5/25/2021
  * License: Public Domain
  */
 
@@ -9,10 +9,32 @@
 
 // test function
 
-var outputEl = document.getElementById("output");
-var new1El = document.createElement("p")
-new1El.innerHTML = "mapresults.";
-var new2El = document.createElement("p")
-new2El.innerHTML = "mapoutput.";
-outputEl.appendChind(new1El);
-outputEl.appendChind(new2El);
+$("activate").click(function(){
+  var t = $("#in1").val();
+// Using the core $.ajax() method
+$.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://www.googleapis.com/youtube/v3",
+    // The data to send (will be converted to a query string)
+    data: {
+            // here is where any data required by the api
+            //   goes (check the api docs)
+            id: pure-episode-351209,
+            api_key: "AIzaSyCLvJMV4dskytV1i8RvjZU2fWTLFaJXOfw",
+          },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        console.log(data);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) {
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+})
